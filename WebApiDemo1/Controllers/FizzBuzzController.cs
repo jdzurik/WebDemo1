d2fed1b37a799534106f1a93dc6e1c59bfbe3af4
John Dzurik <j@dzurik.com>
2021-05-24 18:20:53 -0400

    Adde a cleaner version that uses an array instead of string manupilation.

--- WebApiDemo1/Controllers/FizzBuzzController.cs
+++ WebApiDemo1/Controllers/FizzBuzzController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace WebApiDemo1.Controllers
 {
@@ -18,7 +19,7 @@ namespace WebApiDemo1.Controllers
             string FizzBuzzStr ="[";
             if (TopEnd < 1 || TopEnd > 100)
             {
-                FizzBuzzStr += "Error out of bounds";
+                FizzBuzzStr += "Error out side of bounds too low or High Must be between 1 and 100.";
             }
             else
             {
@@ -42,6 +43,39 @@ namespace WebApiDemo1.Controllers
             }
             FizzBuzzStr += "]";
             return FizzBuzzStr;
+        }
+
+        [Route("ArrayVersion/{TopEnd}")]
+        [HttpGet]
+        public String ArrayVersion(int TopEnd)
+        {
+            
+            if (TopEnd < 1 || TopEnd > 100)
+            {
+                return "Error out side of bounds too low or High Must be between 1 and 100.";
+            }
+            else
+            {
+                string[] FizzBuzzArry = new string[TopEnd];
+                for (int i = 1; i < TopEnd +1; i++)
+                {
+                    
+                    if ((i % 3) != 0 && (i % 5) != 0) FizzBuzzArry[i-1] = i.ToString();
+
+                    if ((i % 3) == 0)
+                    {
+                        FizzBuzzArry[i-1] = "Fizz";
+                    }
+
+                    if ((i % 5) == 0)
+                    {
+                        FizzBuzzArry[i-1] += "Buzz";
+                    }
+                }
+                return JsonConvert.SerializeObject(FizzBuzzArry);
+            }
+            
+            
         }
     }
 }
